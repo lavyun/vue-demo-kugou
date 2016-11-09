543c1052fa71dffa55d50d256d936935bd1cf22a
lavyun <lavyun@163.com>
2016-11-09 12:50:30 +0800

    finish singer_info

--- src/views/singer_info.vue
+++ src/views/singer_info.vue
@@ -1,8 +1,94 @@
 <template>
-  <div>
+  <div class="rank-info-content plist-info">
+    <div class="rank-banner-wrap" :style="{'background':'url('+imgSrc+') no-repeat center'}">
+      <div class="rank-status container">
+        <p>{{updateTime}}</p>
+      </div>
+    </div>
 
+    <div class="plist-desp container">
+      <p class="plist-desp-p" :class="{'plist-desp-hide': hideDesp }">{{desp}}</p>
+      <img src="../../static/close_icon.png" alt="" @click="toggleDesp" class="plist-desp-icon" v-if="hideDesp">
+      <img src="../../static/open_icon.png" alt="" @click="toggleDesp" class="plist-desp-icon" v-else>
+    </div>
+    <div class="plist-desp-bottom" style="width: 100%;height: 5px;background-color: #f1f1f1"></div>
+
+    <div class="rank-info-list">
+      <mt-cell v-for="(item,index) in songList" :title="item.title" @click.native="playAudio(index)">
+        <img src="../../static/download_icon.png" alt="" width="20" height="20">
+      </mt-cell>
+    </div>
   </div>
 </template>
-<script>
+<script type="es6">
+  import { Indicator } from 'mint-ui'
+  export default {
+    data(){
+      return {
+        imgSrc:'',
+        title:'',
+        songList:[],
+        updateTime:'',
+        desp:'',
+        hideDesp:true
+      }
+    },
+    //通过路由的before钩子解除router-view缓存限制
+    beforeRouteEnter (to, from, next) {
+      next(vm => {
+        vm.$store.commit('showHead');
+        Indicator.open({
+          text: '加载中...',
+          spinnerType: 'snake'
+        });
+        vm.get();
+      })
+    },
+    beforeRouteLeave(to,from,next){
+      this.$store.commit('hideHead');
+      next()
+    },
+    methods:{
+      get(){
+        var infoID=this.$route.params.id;
+        this.$http.get('http://cs003.m2828.com/demo/searchIT/proxy.php?val=&url1=http://m.kugou.com/singer/info/&url2='+infoID).then((res)=>{
+          Indicator.close()
 
+          this.parseList(res.data)
+        })
+      },
+      parseList(data){
+        var div=document.createElement('div');
+        div.innerHTML=data;
+        this.imgSrc=div.querySelector('#imgBoxInfo img').src;
+        this.title=div.querySelector('.page-title').innerText;
+        this.desp=div.querySelector('#introBox p').innerText;
+        this.$store.commit('setHeadTitle',this.title);
+        var list=div.querySelectorAll('.panel-songslist-item');
+        this.songList = [];
+        for(var i=0;i<list.length;i++){
+          var song={};
+          var title=list[i].querySelector('.panel-songs-item-name span').innerText;
+          var hash=list[i].id.substr(6);
+          song.title=title;song.hash=hash;
+          this.songList.push(song)
+        }
+      },
+      playAudio(index){
+        console.log(this.songList[index].hash)
+        this.$http.get('http://cs003.m2828.com/phps/getKugouSong.php?hash='+this.songList[index].hash).then((res)=>{
+          //var url=JSON.parse(res.data);
+          var url=JSON.parse(res.data).url;
+          console.log(url)
+          this.$store.commit('setUrl',url);
+        })
+      },
+      toggleDesp(){
+        this.hideDesp=!this.hideDesp;
+      }
+    }
+  }
 </script>
+<style scoped>
+  .rank-banner-wrap{height: 250px;}
+</style>
